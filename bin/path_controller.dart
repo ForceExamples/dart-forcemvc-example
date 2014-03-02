48f4f68f3248c72eadc7c17944bd636e322a2c3f
joris hermans <JHermans85@hotmail.com>
2014-03-02 12:26:23 +0100

    addin more info on the index page

--- bin/path_controller.dart
+++ bin/path_controller.dart
@@ -3,7 +3,7 @@ part of example_forcedart;
 @Controller()
 class PathController {
   
-  @RequestMapping(value: "/var/{var1}")
+  @RequestMapping(value: "/var/{var1}/")
   String variable(req, Model model) {
     model.addAttribute("variable", req.path_variables['var1']);
     return "pathvar";
