8ca249c82cd880eccc3a18a5e7f1e4e689ad976f
Scorpiion <Robert@dartvoid.com>
2014-04-25 03:53:43 +0200

    Restructure and some changes to make the code easier to read and to run both on localhost and on DartVoid.

---
+++ client/web/client.dart
@@ -0,0 +1,7 @@
+import 'dart:async';
+import 'dart:html';
+
+// This is a client side dart script needed by dart force in order to 
+// bootstrap the client side of the application (?) 
+
+void main() {}
