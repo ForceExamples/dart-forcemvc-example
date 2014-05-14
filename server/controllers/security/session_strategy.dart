3df910703e5c6504a968e56d4dc3825bb5fba1e8
joris hermans <JHermans85@hotmail.com>
2014-05-14 17:29:47 +0200

    security change

--- server/controllers/security/session_strategy.dart
+++ server/controllers/security/session_strategy.dart
@@ -2,7 +2,7 @@ part of example_forcedart;
 
 class SessionStrategy extends SecurityStrategy {
   
-  bool checkAuthorization(HttpRequest req) {
+  bool checkAuthorization(HttpRequest req, data) {
     HttpSession session = req.session;
     return (session["user"]!=null);
   }   
