bdce7cf9a219734fdd9ccb1c174aebb51f82cf39
joris hermans <JHermans85@hotmail.com>
2014-05-20 12:14:09 +0200

    making the setup code easier

--- server/server.dart
+++ server/server.dart
@@ -2,9 +2,8 @@ library example_forcedart;
 
 import 'dart:io';
 import 'dart:async';
-import 'package:forcemvc/force_mvc.dart';
-import 'package:logging/logging.dart' show Logger, Level, LogRecord;
 import 'dart:math';
+import 'package:forcemvc/force_mvc.dart';
 
 part 'controllers/post_controller.dart';
 part 'controllers/login_controller.dart';
@@ -16,15 +15,7 @@ part 'controllers/random_interceptor.dart';
 
 part 'controllers/security/session_strategy.dart';
 
-final Logger log = new Logger('ForceMVC');
-
 void main() { 
-  // Set up logger.
-  Logger.root.level = Level.ALL;
-  Logger.root.onRecord.listen((LogRecord rec) {
-    print('${rec.level.name}: ${rec.time}: ${rec.message}');
-  });
-  
   // Setup what port to listen to 
   var portEnv = Platform.environment['PORT'];
   var port = portEnv == null ? 8080 : int.parse(portEnv);
@@ -36,6 +27,8 @@ void main() {
                                    clientFiles: '../client/',
                                    clientServe: serveClient,
                                    views: "views/");
+  // Set up logger.
+  server.setupConsoleLog();
   
   // Setup session strategy
   server.strategy = new SessionStrategy();
