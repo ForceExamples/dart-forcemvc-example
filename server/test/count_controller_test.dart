9d977aa904ae919e3ca09da57590e82ece6b6236
joris hermans <JHermans85@hotmail.com>
2014-07-11 21:02:40 +0200

    testing a controller

---
+++ server/test/count_controller_test.dart
@@ -0,0 +1,24 @@
+import 'package:unittest/unittest.dart';
+import 'package:unittest/mock.dart';
+import 'package:forcemvc/force_mvc.dart';
+import '../server.dart';
+import 'dart:async';
+import 'dart:io';
+
+class MockHttpRequest extends Mock implements HttpRequest {}
+
+main() {  
+  // First tests!  
+  var countController = new CountController();
+  
+  Model model = new Model();
+  ForceRequest req = new ForceRequest(new MockHttpRequest());
+  
+  test('testing the count controller', () {
+      var view = countController.countMethod(req, model);
+      expect(view, "count");
+      expect(model.getData()["count"], "2");
+  });
+  
+ 
+}
