75e65f54dcfadd60065358042d4f80946bfeed5a
joris hermans <JHermans85@hotmail.com>
2014-07-15 15:35:28 +0200

    update testing example

--- server/test/count_controller_test.dart
+++ server/test/count_controller_test.dart
@@ -1,17 +1,14 @@
 import 'package:unittest/unittest.dart';
-import 'package:unittest/mock.dart';
 import 'package:forcemvc/force_mvc.dart';
+import 'package:forcemvc/test.dart';
 import '../server.dart';
-import 'dart:io';
-
-class MockHttpRequest extends Mock implements HttpRequest {}
 
 main() {  
   // First tests!  
   var countController = new CountController();
   
   Model model = new Model();
-  ForceRequest req = new ForceRequest(new MockHttpRequest());
+  MockForceRequest req = new MockForceRequest();
   
   test('testing the count controller', () {
       var view = countController.countMethod(req, model);
